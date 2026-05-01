e448888e2a3e463b0d9654f272bb71f69d91dce7
SvenWollinger <51021531+SvenWollinger@users.noreply.github.com>
2026-05-01 15:20:53 +0200

    Code Cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -4,27 +4,28 @@ import styles from "./App.module.css";
 const EXAMPLE_URL: string = "https://bplace.art/?pixel=676718,459523";
 
 function App() {
-	const [input, setInput] = React.useState(EXAMPLE_URL);
+	const [input, setInput] = React.useState<string>(EXAMPLE_URL);
 
 	const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => { setInput(e.target.value); };
 	const setExampleUrl = () => { setInput(EXAMPLE_URL); };
 
 	let output: React.JSX.Element;
 
-	const inputUrl = new URL(input);
-	const pixelParam = inputUrl.searchParams.get("pixel");
+	const inputUrl: URL = new URL(input);
+	const pixelParam: string | null = inputUrl.searchParams.get("pixel");
 	if(!pixelParam) {
 		output = <div>Invalid URL, click <button onClick={setExampleUrl}>here</button> to load an Example URL</div>;
 	} else {
-		const parts = pixelParam.split(",");
-		const tileX = Math.floor(parseInt(parts[0]) / 512);
-		const tileY = Math.floor(parseInt(parts[1]) / 512);
+		const parts: string[] = pixelParam.split(",");
+		const tileX: number = Math.floor(parseInt(parts[0]) / 512);
+		const tileY: number = Math.floor(parseInt(parts[1]) / 512);
 
 		const URL = `https://tiles.bplace.art/tile-images/${tileX}_${tileY}.png`;
+		const handleOpenURL = () => { window.open(URL, "_blank"); };
 		output = (
 			<>
-				<div><a href={URL} target="_blank">Open Tile ({URL})</a></div>
-				<div><img onError={() => {}} className={styles.tile} src={URL} alt="Tile Image"/></div>
+				<div><button onClick={handleOpenURL} >Open Tile</button></div>
+				<div><img className={styles.tile} src={URL} alt="Tile Image"/></div>
 				<div><span>If the image fails to load inspect the url and make sure there are pixels at the location.</span></div>
 			</>
 		);
